ae42b47e9f550a2cd3aceea0eec131620cf377da
MateoGiuffra <142268114+MateoGiuffra@users.noreply.github.com>
2024-06-06 19:54:13 -0300

    Revisar resize, hacer ejercicios de ArrayList

--- tp-10/ArrayList/ArrayList.cpp
+++ tp-10/ArrayList/ArrayList.cpp
@@ -30,14 +30,15 @@ int lengthAL(ArrayList xs){
     return xs->cantidad; 
 }
 int get(int i, ArrayList xs){
-// Devuelve el iésimo elemento de la lista.
-    if  (i>0 && i <= xs->cantidad){
+// Devuelve el _ elemento de la lista.
+    if  (i>=0 && i < xs->cantidad){
         return xs->elementos[i];
     }
-    return 0; 
+    cout << "Indice invalido."<<endl;
+    //exit(1); 
 }
 void set(int i, int x, ArrayList xs){
-// Reemplaza el iésimo elemento por otro dado.
+// Reemplaza el _ elemento por otro dado.
     if (i > 0 && i < xs->capacidad){
         xs->elementos[i] = x; 
     }
@@ -46,71 +47,81 @@ void resize(int capacidad, ArrayList xs){
 // Decrementa o aumenta la capacidad del array.
 // Nota: en caso de decrementarla, se pierden los elementos del final de la lista.
         int* resizeList = new int[capacidad]; 
-        if(xs->capacidad =< capacidad){
+        if(xs->capacidad <= capacidad){
             //incrementar 
             for (int i =0; i < xs->cantidad; i++){
                 resizeList[i] = xs->elementos[i];
-            }
-            xs->elementos = resizeList; 
-            xs->capacidad = capacidad;
-            // aca la cantidad de elementos no cambia 
+            }//la cantidad queda igual
         }else{
             //decrementar 
             int cantidadNueva = xs->cantidad < capacidad ? xs->cantidad : capacidad;  //el minimo entre la capacidad y la cantidad de elementos. 
             for (int i =0; i < cantidadNueva; i++){
                 resizeList[i] = xs->elementos[i];
             }
-            xs->capacidad = capacidad;      // cargas nueva capacidad
-            xs->elementos = resizeList;     // cargas la lista hasta la capacidad disponible 
-            xs->cantidad  = cantidadNueva
+            xs->cantidad  = cantidadNueva;
         }
+        xs->capacidad = capacidad;      // cargas nueva capacidad
+        xs->elementos = resizeList;     // cargas la lista hasta la capacidad disponible
+        delete[] xs->elementos;
+
 }
-
-//para comparar despues:
-// void resize(int capacidad, ArrayList xs) {
-//     int* resizeList = new int[capacidad];
-
-//     if (xs->capacidad <= capacidad) {
-//         for (int i = 0; i < xs->cantidad; i++) {
-//             resizeList[i] = xs->elementos[i];
-//         }
-//     } else {
-//         int cantidadNueva = xs->cantidad < capacidad ? xs->cantidad : capacidad;
-//         for (int i = 0; i < cantidadNueva; i++) {
-//             resizeList[i] = xs->elementos[i];
-//         }
-//         xs->cantidad = cantidadNueva;
-//     }
-
-//     delete[] xs->elementos;
-//     xs->elementos = resizeList;
-//     xs->capacidad = capacidad;
-// }
-
-
 
 
 void add(int x, ArrayList xs){
 // Agrega un elemento al final de la lista.
     if (xs->capacidad == xs->cantidad){
-        return xs; 
+        resize(xs->capacidad*2,xs);
     }else{
-        xs->elementos[xs->cantidad + 1] = x;
-        xs->cantidad++; 
-        return xs; 
+        xs->elementos[xs->cantidad] = x;
+        xs->cantidad++;
     }
 }
+
+
 void remove(ArrayList xs){
 // Borra el último elemento de la lista
     if (xs->cantidad > 0){
-        xs->cantidad--;
+        xs->cantidad --; 
+        for (int i = xs->cantidad +1; i < xs->capacidad; i++){
+          xs->elementos[i-1] = xs->elementos[i]; 
+        }
     }else{
         cout <<"La lista esta vacia"<<endl;
     }
     
 }
 
+
 int main(){
     ArrayList arrayTest = newArrayList();
-    cout<< lengthAL(arrayTest) << endl;
+    cout<< lengthAL(arrayTest) << endl; // deberia de dar 0  | True
+    add(100,arrayTest);
+    add(150,arrayTest);
+    add(200,arrayTest);
+    cout<< lengthAL(arrayTest) << endl; // deberia de dar 3  | True
+    cout<< lengthAL(arrayTest) << endl; // deberia de dar 2  | True
+    
+    cout << "El indice 0 vale:" <<get(0,arrayTest) <<endl;    // deberia de dar 100
+    cout << "El indice 1 vale:" <<get(1,arrayTest) <<endl;    // deberia de dar 150
+    cout << "El indice 2 vale:" <<get(2,arrayTest) <<endl;    // deberia de dar 200
+    // cout << "El indice 3 vale:" <<get(3,arrayTest) <<endl;    // no deberia de dar nada, porque solo se agregaron 3 elementos. 
+    remove(arrayTest);
+    cout << "Despues de borrar el ultimo elemento del ArrayList quedaria:" <<endl; 
+    cout << "El indice 0 vale:" <<get(0,arrayTest) <<endl;    // deberia de dar 100
+    cout << "El indice 1 vale:" <<get(1,arrayTest) <<endl;    // deberia de dar 150
+    cout << "El indice 2 vale:" <<get(2,arrayTest) <<endl;    // no deberia de dar nada, porque se borro el ultimo elemento. 
+    //cout << "El indice 3 vale:" <<get(3,arrayTest) <<endl;    // no deberia de dar nada, porque solo hay 2. 
+
+    cout << "Se agregan los elementos 200, 250, 300 al ArrayList." <<endl;
+    add(200,arrayTest);
+    add(250,arrayTest);
+    add(300,arrayTest);
+    cout<< "por ende esto deberia de ser cierto 5 == " << lengthAL(arrayTest) <<endl; 
+    cout << "La capacidad del arrayTest es de: " << arrayTest->capacidad << "." << endl;  
+    resize(2,arrayTest); 
+    cout << "Despues de un resize a 2, la capacidad es de: " << arrayTest->capacidad << "." << endl;
+    cout << "Y no deberia de mostrarme que hay en el indice 4: " << get(4,arrayTest) << " Pero si del 1: " << get(1,arrayTest) <<endl; 
+
+    
+
 }
--- tp-10/ArrayList/ArrayList.h
+++ tp-10/ArrayList/ArrayList.h
@@ -15,9 +15,9 @@ ArrayList newArrayListWith(int capacidad);
 int lengthAL(ArrayList xs);
 // Devuelve la cantidad de elementos existentes.
 int get(int i, ArrayList xs);
-Devuelve el iésimo elemento de la lista.
+//Devuelve el _ elemento de la lista.
 void set(int i, int x, ArrayList xs);
-// Reemplaza el iésimo elemento por otro dado.
+// Reemplaza el _ elemento por otro dado.
 void resize(int capacidad, ArrayList xs);
 // Decrementa o aumenta la capacidad del array.
 // Nota: en caso de decrementarla, se pierden los elementos del final de la lista.
